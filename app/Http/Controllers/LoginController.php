ff29c51504c3599a46201e085b06a1015e4b8cb8
sintsovamy <msintsova18@gmail.com>
2023-11-11 14:44:32 +0700

    add login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function seen()
-    {
-        return 'Страница логина';
-    }
-
     public function login(Request $request)
     {
-        if (Auth::attempt(['bearer_token' => $request->token]);
-    
+        if(Auth::attempt(['email'=> $request->email, 'password'=> $request->password]))
+        {
+            $user = Auth::user();
+            $token = $user->createToken('user_token')->accessToken;
+	    return response()->json([
+		    'success' => true,
+		    'code' => 200,
+		    'message' => 'Success', 
+		    'token'=> $token->token
+	    ], 200);
+	} else {
+            return response()->json([
+		    'success' => false,
+		    'code' => 401,
+		    'message' => 'Authorization failed'
+	    ], 401);
+	}
+    }
 }
