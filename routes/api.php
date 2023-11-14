26a104a3ee19231458560d5836d8b09a0f183a9f
sintsovamy <msintsova18@gmail.com>
2023-11-14 23:40:29 +0700

    all files actions done

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,11 @@ use App\Http\Controllers\LogoutController;
 Route::post('/authorization', [LoginController::class, 'login'])->name('login');   //+
 Route::post('/registration', [RegisterController::class, 'signup']);   //+
 Route::middleware('auth:sanctum')->get('/logout', [LogoutController::class, 'logout']);   //+
-Route::post('/files', [FilesController::class, 'upload']);    //+
-Route::get('/files', [FilesController::class, 'index']);    //+
+Route::middleware('auth:sanctum')->post('/files', [FilesController::class, 'upload']);    //+
+Route::middleware('auth:sanctum')->get('/files', [FilesController::class, 'index']);    //+
 Route::middleware('auth:sanctum')->post('/files/{file}', [FilesController::class, 'edit']);   //+
 Route::middleware('auth:sanctum')->post('/files/{file}', [FilesController::class, 'delete']);
-Route::get('/files/{file}', [FilesController::class, 'download']);
+Route::middleware('auth:sanctum')->get('/files/{file}', [FilesController::class, 'download']);   //+
 Route::middleware('auth:sanctum')->post('/files/{file}/accesses', [AccessesController::class, 'add']);
 Route::middleware('auth:sanctum')->delete('/files/{file}/accesses', [AccessesController::class, 'delete']);
 Route::middleware('auth:sanctum')->get('/files/disk', [DisksController::class, 'show']);
